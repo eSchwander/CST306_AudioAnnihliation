9588b62c9b3668f0b4cc9debe6f8cefec3a4f2a7
Jhazgorg <eschwander@csumb.edu>
2015-11-08 14:53:03 -0800

    added Music and SongSelection script

---
+++ Assets/Scripts/SongSelection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class SongSelection : MonoBehaviour {
+	string [] songs;
+	string assestsPath;
+
+	// Use this for initialization
+	void Start () {
+
+		assestsPath = Application.dataPath;
+		songs = Directory.GetFiles(assestsPath + "\\Music", "*.mp3");
+
+		foreach (string path in songs)
+			Debug.Log (path);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
